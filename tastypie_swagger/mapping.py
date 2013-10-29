8d12c5b067d2597997b2537bcb4c4fa6bcd39fee
Nathan Keilar <nathank@huntedhive.com>
2013-10-29 04:50:52 -0700

    extend standard api endpoints docs with custom parameters used for filtering

--- tastypie_swagger/mapping.py
+++ tastypie_swagger/mapping.py
@@ -148,6 +148,23 @@ class ResourceSwaggerMapping(object):
                     required=False,
                     description=force_unicode(desc),
                 ))
+
+        # For non-standard API functionality, allow the User to declaritively
+        # define their own filters, along with Swagger endpoint values.
+        # Minimal error checking here. If the User understands enough to want to
+        # do this, assume that they know what they're doing.
+        if hasattr(self.resource.Meta, 'custom_filtering'):
+            for name, field in self.resource.Meta.custom_filtering.items():
+                parameters.append(self.build_parameter(
+                        paramType = 'query',
+                        name = name,
+                        dataType = field['dataType'],
+                        required = field['required'],
+                        description = unicode(field['description'])
+                        ))
+
+        return parameters
+
         if 'filtering' in self.schema and method.upper() == 'GET':
             for name, field in self.schema['filtering'].items():
                 # Integer value means this points to a related model
